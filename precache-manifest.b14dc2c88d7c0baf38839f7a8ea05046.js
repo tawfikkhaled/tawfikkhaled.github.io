d0be27b065674b36e4a3fc8bd6659cc7ce957caa
tawfik <taoufikkhaled103@gmail.com>
2021-10-15 19:23:58 +0200

    site example

---
+++ precache-manifest.b14dc2c88d7c0baf38839f7a8ea05046.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c91e301cfb0bd1f895ccf10c319279cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bec2994d0a501bff9ae",
+    "url": "/static/css/main.b8cee5fb.chunk.css"
+  },
+  {
+    "revision": "66057293e9aff8f36eaf",
+    "url": "/static/js/2.1152b9cd.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/static/js/2.1152b9cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bec2994d0a501bff9ae",
+    "url": "/static/js/main.4d2c199a.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
